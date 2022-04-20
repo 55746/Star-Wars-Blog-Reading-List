264d1d4c2c6911f52a94e100ab5c21743bcbdb1f
55746 <Lawhead.gavin@live.jobcorps.org>
2022-04-20 17:38:38 +0000

    lookin good

--- src/js/component/Details.js
+++ src/js/component/Details.js
@@ -5,27 +5,33 @@ export const Details = () => {
   const { store, actions } = useContext(Context);
   console.log(store);
   return (
-    <div className="row">
-      <div>
-        <img
-          src={store.img + list.uid + ".jpg"}
-          className="card-img-top"
-          alt="..."
-        />
-        <div key={index} className="card text-center">
-          <div className="card-header">Featured</div>
-          <div className="card-body">
-            <h5 className="card-title">
-              {store.singleCharacter.result.properties.name}
-            </h5>
-            <p className="card-text">{}</p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+    <>
+      {store.singleCharacter.result ? (
+        <div className="row">
+          <div>
+            <img
+              src={store.img + store.singleCharacter.result.uid + ".jpg"}
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card text-center">
+              <div className="card-header">Featured</div>
+              <div className="card-body">
+                <h5 className="card-title">
+                  {store.singleCharacter.result.properties.name}
+                </h5>
+                <p className="card-text">{}</p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+              <div className="card-footer text-muted">2 days ago</div>
+            </div>
           </div>
-          <div className="card-footer text-muted">2 days ago</div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <h1>Loading</h1>
+      )}
+    </>
   );
 };
